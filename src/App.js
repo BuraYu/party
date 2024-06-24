534dbbe66646b04d86d63892b4183ace4ec10302
BuraYu <burak.yueksel@hotmail.com>
2024-06-24 23:47:28 +0200

    deleted all the unnecesary files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="flex items-center justify-center h-screen font-poppins">
-      <h1 className="text-center text-4xl">Party Planner!</h1>
+      <h1 className="text-center text-4xl font-black">Party Planner!</h1>
     </div>
   );
 }
